64e31b41f47f3d2a9d27d5b43ff2a60229a6d0b2
unknown <>
2016-12-19 14:01:59 +0900

    add error exit etc

--- 01_to_QRCode.rb
+++ 01_to_QRCode.rb
@@ -1,12 +1,22 @@
 require 'cairo'
 
-format = Cairo::FORMAT_ARGB32
-#set the size of QRCode
-width = 400
-height = 400
 #set the width and height of a dot
 dot = 10
 
+#pixel
+w_dots = "set the number of dots of the width"
+h_dots = "set the number of dots of the height"
+
+if !w_dots.is_a?(Integer) || !h_dots.is_a?(Integer)
+	puts "set the correct number of dots"
+	exit(1)
+end
+
+#set the size of QRCode
+width = dot * w_dots
+height = dot * h_dots
+
+format = Cairo::FORMAT_ARGB32
 surface = Cairo::ImageSurface.new(format,width,height)
 context = Cairo::Context.new(surface)
 
@@ -16,10 +26,10 @@ context.rectangle(0,0,width,height)
 context.fill
 
 x = y = 0
-for i in 0 .. 30
+for i in 0 .. h_dots-1
 	#input the 01 data
 	qr_row = gets.chomp
-	for j in 0 .. qr_row.length - 1
+	for j in 0 .. w_dots-1
 		if qr_row[j] == "1"
 			context.set_source_rgb(0,0,0)  #dot color(default:black)
 			context.rectangle(x,y,dot,dot)
